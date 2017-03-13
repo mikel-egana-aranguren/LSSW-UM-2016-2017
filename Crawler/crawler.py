0bad30237dc9be9b25c1fad2bacd6fb6637a4b78
mikel-egana-aranguren <mikel.egana.aranguren@gmail.com>
2017-03-13 15:41:31 +0100

    Crawler

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -1,17 +1,32 @@
 import rdflib
 
-#g = rdflib.Graph()
-#result = g.parse("http://purl.uniprot.org/uniprot/P05067")
+def visit(uri,graph,count,maxcount):
+  print "Visiting resource: ",uri  
+  print count,maxcount
+  if count < maxcount:
+  #if len(graph) < size:
+    graph.parse(uri)
+    count += 1
+    for subj, pred, obj in graph:
+      print "Obtained Triple: ",subj, pred, obj
+      if (type(obj) == rdflib.term.URIRef):
+	visit(str(obj),graph,count,maxcount)
 
-g = rdflib.Graph()
-result = g.parse("http://sameas.org/rdf?uri=http://identifiers.org/go/GO:0006915")
+
+# Config
+uri = "http://purl.uniprot.org/uniprot/P05067"
+size = 100
+count = 0
+maxcount = 10
+
+graph = rdflib.Graph()
+visit(uri,graph,count,maxcount)
+
+print "Graph size: ", len(graph)
+graph.serialize("result.ttl",format="turtle")
 
 
-
-#g = rdflib.Graph()
-#result = g.parse("http://sameas.org/rdf?uri=http://dbpedia.org/resource/London")
-
-for subj, pred, obj in g:
-   print subj, pred, obj
+# uri != "http://www.w3.org/2000/01/rdf-schema-more"
    
-#g.serialize("result.ttl",format="turtle")
+
+
