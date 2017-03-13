58344ba3dfe2cd266b5a62c38b9e6b8298972f56
mikel-egana-aranguren <mikel.egana.aranguren@gmail.com>
2017-03-13 22:23:06 +0100

    crawler

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -2,22 +2,26 @@ import rdflib
 
 def visit(uri,graph,count,maxcount):
   print "Visiting resource: ",uri  
-  print count,maxcount
+  print count,maxcount,len(graph)
   if count < maxcount:
-  #if len(graph) < size:
-    graph.parse(uri)
-    count += 1
-    for subj, pred, obj in graph:
-      print "Obtained Triple: ",subj, pred, obj
-      if (type(obj) == rdflib.term.URIRef):
-	visit(str(obj),graph,count,maxcount)
+    try:
+      g = rdflib.Graph()
+      g.parse(uri)
+      count += 1
+      for subj, pred, obj in g:
+	print "Obtained Triple: ",subj, pred, obj
+	if (type(obj) == rdflib.term.URIRef):
+	  graph.add(subj, pred, obj)
+	  visit(str(obj),graph,count,maxcount)
+    except:
+      pass
 
 
 # Config
 uri = "http://purl.uniprot.org/uniprot/P05067"
 size = 100
 count = 0
-maxcount = 10
+maxcount = 2
 
 graph = rdflib.Graph()
 visit(uri,graph,count,maxcount)
@@ -26,7 +30,7 @@ print "Graph size: ", len(graph)
 graph.serialize("result.ttl",format="turtle")
 
 
-# uri != "http://www.w3.org/2000/01/rdf-schema-more"
+# and (str(pred) == "http://www.w3.org/2000/01/rdf-schema#seeAlso")
    
 
 
